ff3559c42ec67e1e129262b93d07e5df088754cb
dong1203 <hambur1203@gmail.com>
2026-02-13 23:08:45 +0900

    feat: add retry example DAG with intentional failure

--- Airflow/dags/retry_example.py
+++ Airflow/dags/retry_example.py
@@ -6,38 +6,37 @@ from airflow.sdk import DAG
 from airflow.providers.standard.operators.python import PythonOperator
 
 # python 함수 정의
-def push_func():
-    return "xcom test"
+def fail_func():
+    raise Exception("의도적 실패!")
 
-def pull_func(**context):
-    value = context['ti'].xcom_pull(task_ids='task_push')
-    print(value)
+def success_func():
+    print("Task succeeded!")
 
 # 2. DAG 정의
 with DAG(
-    dag_id="xcom_example",  
+    dag_id="retry_example",  
     default_args={                   
         "depends_on_past": False,
         "retries": 2,
         "retry_delay": timedelta(seconds=5),
     },
-    description="XCom 예제", 
+    description="Retry 예제", 
     schedule=timedelta(days=1),      
     start_date=datetime(2026, 1, 1), 
     catchup=False,   
-    tags=["task_xcom"],
+    tags=["task_retry"],
 ) as dag:
 
     # Task 정의 (Operator 사용)
     task_1 = PythonOperator(
-        task_id="task_push",
-        python_callable=push_func,
+        task_id="task_success",
+        python_callable=success_func,
         retries=3,
     )
 
     task_2 = PythonOperator(
-        task_id="task_pull",
-        python_callable=pull_func,
+        task_id="task_fail",
+        python_callable=fail_func,
         retries=3,
     )
 
